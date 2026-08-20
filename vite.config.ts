c122619afe7b8009be7644980ee8b02a58a71722
shay <43248357+shayypy@users.noreply.github.com>
2026-08-20 08:33:08 -0500

    fix: proper react plugin import

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("./app", import.meta.url)),
